ccbaf76cf13eafd4de2fbd85f8334c0abba0bf81
TechMrs <kemilat50@gmail.com>
2024-04-09 18:48:59 +0800

    style: made landingpage and homepage responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ function Homepage() {
   return (
     <>
       <div className=" homebg h-full flex items-center justify-center">
-        <div className=" bg-gray-800 bg-opacity-90 flex flex-col gap-[3em] items-center justify-center w-[50%] py-10 rounded-tl-3xl rounded-br-3xl">
-          <h2 className=" mx-auto p-10 w-[80%] text-center   md:text-3xl lg:text-5xl sm:text-sm text-[#e1e9e5] font-extrabold ">
+        <div className=" bg-gray-800 bg-opacity-90 flex flex-col sm:gap-[1em] gap-[3em] items-center justify-center sm:w-[90%] md:w-[80%] lg:w-[50%] p-10 rounded-tl-3xl rounded-br-3xl">
+          <h2 className=" mx-auto sm:w-full md:w-full lg:w-[80%] text-center   md:text-3xl lg:text-5xl sm:text-2xl text-[#e1e9e5] font-extrabold ">
             NEED AN ACCOUNT?
           </h2>
 
-          <button className=" text-gray-50 bg-gradient-to-r from-[#1e2855] to-[#381d53] text-5xl py-5 px-8 rounded-md font-bold lg:hover-transform lg:hover:scale-105">
+          <button className=" text-gray-50 bg-gradient-to-r from-[#1e2855] to-[#381d53]  md:text-3xl lg:text-5xl sm:text-xl py-5 px-8 rounded-md font-bold lg:hover-transform lg:hover:scale-105">
             <Link to="/register">Sign Up</Link>
           </button>
         </div>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -14,18 +14,18 @@ const LandingPage = () => {
   return (
     <div className="bg-[#10416A]">
       <div className="flex items-center justify-center h-screen bg-hero-pattern bg-center bg-contain bg-no-repeat">
-        <section className="flex flex-col gap-10 justify-center items-center text-5xl">
-          <h1 className="text-[#e1e9e5] py-10 px-8 font-bold bg-[#318a96] rounded-md">
+        <section className="flex flex-col gap-10 justify-center items-center sm:text-2xl md:text-4xl lg:text-5xl">
+          <h1 className=" text-[#e1e9e5] sm:py-4 sm:px-4 md:py-10 md:px-8 lg:py-10 lg:px-8 font-bold bg-[#318a96] rounded-md">
             Success Logging In!
           </h1>
 
-          <p className=" text-gray-50 bg-gradient-to-r from-[#1e2855] to-[#381d53] py-5 px-8 rounded-md font-bold lg:hover-transform lg:hover:scale-105">
+          <p className=" text-gray-50 bg-gradient-to-r from-[#1e2855] to-[#381d53] sm:py-4 sm:px-4 md:py-5 md:px-8 lg:py-5 lg:px-8 rounded-md font-bold lg:hover-transform lg:hover:scale-105">
             Welcome To KodeMaven
           </p>
 
           <button
             onClick={handleClick}
-            className=" text-gray-50 bg-gradient-to-r from-[#318a96] to-[#381d53] py-5 px-8 rounded-md font-bold lg:hover-transform lg:hover:scale-105"
+            className=" text-gray-50 bg-gradient-to-r from-[#318a96] to-[#381d53] sm:py-4 sm:px-4 md:py-5 md:px-8 lg:py-5 lg:px-8 rounded-md font-bold lg:hover-transform lg:hover:scale-105"
           >
             Logout
           </button>
